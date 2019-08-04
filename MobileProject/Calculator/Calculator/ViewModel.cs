d5ff3c6ce8216eb3f481a65bffa6272fd1fd8696
AniHakobyan <annie2016l@gmail.com>
2019-08-04 16:57:34 +0400

    Implemented ICommand interface

---
+++ MobileProject/Calculator/Calculator/ViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Calculator
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _text;
+
+        public ViewModel()
+        {
+            ClickCommand = new BaseCommand(ClickExecute);
+        }
+
+        public string Text
+        {
+            get { return _text; }
+            set
+            {
+                if (_text != value)
+                {
+                    _text = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs(nameof(Text)));
+                }
+            }
+        }
+
+        public ICommand ClickCommand { get; }
+        public ICommand EqualCommand { get; }
+        void ClickExecute(object param)
+        {
+            if (param is string text)
+            {
+                Text += text;
+            }
+        }
+
+        void ClickEqual(object param)
+        {
+            //if (param.ToString() == "=")
+            //{
+            Text = "00000000";
+            // }     
+        }
+    }
+}
